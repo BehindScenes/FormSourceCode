74048b2d18511038343f602682e6a146dd55d496
Abd El Rahmane <ghano.2004.usma@gmail.com>
2025-11-22 12:51:20 +0100

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('showLogin');
+});
+
 // Route Syntax: Route::method(get,post,put,patch,delete, [ControllerName::class, 'Method inside the controller'])->name('routeName');
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
